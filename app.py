0b5da9a5da914f79308e0ff9e292f860791cbdce
Sai Prudhvi Neelakantam | GeekInData <nsprudhvi1@gmail.com>
2024-06-04 22:51:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,8 @@ from streamlit_webrtc import webrtc_streamer
 
 def main():
     st.title("Live Camera Stream")
-    
-    rtc_configuration = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 
-    media_stream_constraints = {
-        "video": True,
-        "audio": False,
-    }
-    
-    webrtc_ctx = webrtc_streamer(
-        key="example",
-        rtc_configuration=rtc_configuration,
-        media_stream_constraints=media_stream_constraints,
-    )
-
-    if webrtc_ctx.video_stream:
-        st.write("Live Camera Stream")
-        st.write(webrtc_ctx.video_stream)
+    webrtc_streamer(key="example")
 
 if __name__ == "__main__":
     main()
