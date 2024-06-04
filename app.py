94c925ad9b3b15cf16693d3765d92811c5581152
Sai Prudhvi Neelakantam | GeekInData <nsprudhvi1@gmail.com>
2024-06-04 22:44:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, ClientSettings
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
@@ -8,19 +8,18 @@ class VideoProcessor(VideoProcessorBase):
 def main():
     st.title("Live Camera Stream")
     
-    client_settings = ClientSettings(
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={
-            "video": True,
-            "audio": False,
-        },
-    )
+    rtc_configuration = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+
+    media_stream_constraints = {
+        "video": True,
+        "audio": False,
+    }
     
     webrtc_ctx = webrtc_streamer(
         key="example",
         video_processor_factory=VideoProcessor,
-        rtc_configuration=client_settings.rtc_configuration,
-        media_stream_constraints=client_settings.media_stream_constraints,
+        rtc_configuration=rtc_configuration,
+        media_stream_constraints=media_stream_constraints,
     )
 
     if webrtc_ctx.video_processor:
